cb385729c641d41af1cf1af41a52692b6e4cb8b0
jimlyons <the_stratigest@hotmail.com>
2019-11-19 11:46:37 -0500

    Added view

---
+++ BusinessLayer/KTBusiness.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIT255_KT_list_builder.BusinessLayer
+{
+    class KTBusiness 
+    {
+        #region ENUMS
+
+        #endregion
+
+        #region FIELDS
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTOR
+        
+        /// <summary>
+        /// Default Constructor: 
+        /// </summary>
+        public KTBusiness()
+        {
+
+        }
+        #endregion
+
+        #region METHODS
+
+        #endregion
+    }
+}
--- PresentationLayer/Views/KTView.xaml.cs
+++ PresentationLayer/Views/KTView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CIT255_KT_list_builder
+namespace CIT255_KT_list_builder.PresentationLayer.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for KTView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class KTView : Window
     {
-        public MainWindow()
+        public KTView()
         {
             InitializeComponent();
         }
